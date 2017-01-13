40f6faa6ccc7e58d3a7d25ee868c70000871eed0
rajula96reddy <rajula96reddy@gmail.com>
2017-01-14 03:38:52 +0530

    First

---
+++ public/javascripts/getCoins.js
@@ -0,0 +1,27 @@
+var request = require('request');
+
+var getCoins = function( callback) {
+    console.log('I am here');
+
+    var options = {
+        url: 'https://api.coinsecure.in/v1/exchange/lastTrade'
+    }
+    console.log(options.url);
+    request.get(options, function(err, response) {
+        console.log(response.body.status+'Is the response body');
+        if (err){
+            return callback(err)
+        }
+        console.log(response.body+'Is the response body');
+        var responseJson = JSON.parse(response.body);
+        if(responseJson.status=='ERROR'){
+            console.log("IN ERROR RESPONSE");
+            callback(null,null);
+        }
+        console.log(response.body+'news is above thissssssssssssssss');
+        var newsData = responseJson.message.avgRate;
+        callback(null, newsData)
+    })
+}
+
+module.exports = getCoins
---
+++ routes/coinsecure.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Dell on 14-01-2017.
+ */
+var express = require('express');
+var getCoins = require('../public/javascripts/getCoins');
+
+var router = express.Router();
+router.get('/', function(req, res, next) {
+    console.log("Hey in Conversation");
+    getCoins(function(err, response) {
+        if (err) {
+            console.log('here I am in error')
+            res.status(500).json(err)
+        } else{
+            console.log('I am here in convo'+response);
+            res.status(200).json(response)
+        }
+    })
+});
+
+module.exports = router;
+
